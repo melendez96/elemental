84bf0daf69695e970216d9ebf53a01af817b64d5
Jesus Melendez <melendez.jsma.gmail.com>
2019-08-23 04:00:01 +0000

    templates ready

---
+++ Smart-Bartender-master/bachelorette_drinks.py
@@ -0,0 +1,112 @@
+## drinks.py
+drink_list = [
+	{
+		"name": "Mojito",
+		"ingredients": {
+			"ron blanco": 0.75,
+			"lemonade": 1.5                    
+		}
+	}, {
+		"name": "Long Island",
+		"ingredients": {
+			"ron blanco": 0.25,
+			"gin": 0.25,
+                        "vodka": 0.25,
+                        "tea": 1.5
+		}
+	}, {
+		"name": "Margarita",
+		"ingredients": {
+			"tequila": 0.75,
+			"lemonade": 1.5
+		}
+	}, {
+		"name": "The one stand",
+		"ingredients": {
+			"vodka": 0.50,
+			"lemonade": 1.75
+		}
+	}, {
+		"name": "Tesito dulce",
+		"ingredients": {
+			"vodka": 0.50,
+			"lemonade": 0.250,
+                        "tea": 1.250
+		}
+	}, {
+		"name": "Tomita (Tom Collins)",
+		"ingredients": {
+			"gin": 0.5,
+			"lemonade": 0.25,
+                        "agua mineral": 1.25
+		}
+	}, {
+		"name": "White Cuba Libre",
+		"ingredients": {
+			"ron blanco": 0.5,
+			"coke": 1.75
+		}
+	},{
+		"name": "Ruso Negron",
+		"ingredients": {
+			"kahlua": 0.325,
+			"vodka": 0.75
+		}
+	},{
+		"name": "Ruso Americanon",
+		"ingredients": {
+			"kahlua": 0.5,
+			"vodka": 0.5,
+                        "coke": 1.25
+		}
+	},{
+		"name": "La mera macha",
+		"ingredients": {
+			"tequila": 0.5,
+			"lemonade": 0.25,
+                        "agua mineral": 1.25
+		}
+	},{
+		"name": "La cucaracha",
+		"ingredients": {
+			"kahlua": 1,
+			"tequila": 1.25
+		}
+	},{
+		"name": "Coca Mexa",
+		"ingredients": {
+			"tequila": 0.5,
+			"lemonade": 0.25,
+                        "coke": 1.25
+		}
+	},
+        {
+		"name": "Los Britanicos",
+		"ingredients": {
+			"gin": 0.5,
+			"lemonade": 0.25,
+                        "coke": 1.50
+		}
+	},{
+		"name": "Bolas Rapidas",
+		"ingredients": {
+			"gin": 0.375,
+			"tequila": 0.375,
+                        "vodka": 0.375,
+                        "lemonade": 1.125
+		}
+	}
+]
+
+drink_options = [
+	{"name": "Ron Blanco", "value": "ron blanco"},
+	{"name": "Gin", "value": "gin"},
+	{"name": "Kahlua", "value": "kahlua"},
+	{"name": "Tequila", "value": "tequila"},
+	{"name": "Vodka", "value": "vodka"},
+	{"name": "Lemonade", "value": "lemonade"},
+	{"name": "Sweet Tea", "value": "tea"},
+	{"name": "Agua Mineral", "value": "agua mineral"},
+        {"name": "Coca Cola", "value": "coke"},
+        {"name": "Agua", "value": "water"}
+]
